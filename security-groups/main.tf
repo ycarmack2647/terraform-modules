a412b496cb45043ec2997d371f8d3b5a6ae61626
shon <yashoncarmack@gmail.com>
2024-08-07 13:05:12 -0400

    correct port 80

--- security-groups/main.tf
+++ security-groups/main.tf
@@ -7,7 +7,7 @@ resource "aws_security_group" "alb_security_group" {
   ingress {
     description = "http access"
     from_port   = 80
-    to_port     = 90
+    to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
