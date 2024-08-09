f5f20724e6c125c3d0fc5cab7dbb3ba4f5d04742
shon <yashoncarmack@gmail.com>
2024-08-09 17:45:50 -0400

    created iam role fix

--- iam-role/main.tf
+++ iam-role/main.tf
@@ -61,5 +61,5 @@ resource "aws_iam_role" "ecs_task_execution_role" {
 # attach ecs task execution policy to the iam role
 resource "aws_iam_role_policy_attachment" "ecs_task_execution_role" {
   role       = aws_iam_role.ecs_task_execution_role.name
-  policy_arn = aws_iam_role.ecs_task_execution_role.arn
+  policy_arn = aws_iam_policy.ecs_task_execution_policy.arn
 }
