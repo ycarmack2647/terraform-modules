a22b9d29a1ad025b15d6caa824c6d5cbf5f98f1e
shon <yashoncarmack@gmail.com>
2024-08-11 08:39:35 -0400

    check

--- ecs/main.tf
+++ ecs/main.tf
@@ -71,7 +71,7 @@ resource "aws_ecs_service" "ecs_service" {
   cluster                            = aws_ecs_cluster.ecs_cluster.id 
   task_definition                    = aws_ecs_task_definition.ecs_task_definition.arn
   platform_version                   = "LATEST"
-  desired_count                      = 2
+  desired_count                      = 1
   deployment_minimum_healthy_percent = 100
   deployment_maximum_percent         = 200
 
